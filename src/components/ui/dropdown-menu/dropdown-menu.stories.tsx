9d76e4d6cead8017e56c799f6d623183b5c51020
Udaloiii <udaloiiisohappy@gmail.com>
2023-09-06 10:31:18 +0300

    Changed dropdown-menu (deleted unnecessary)

--- src/components/ui/dropdown-menu/dropdown-menu.stories.tsx
+++ src/components/ui/dropdown-menu/dropdown-menu.stories.tsx
@@ -1,6 +1,10 @@
 import { Meta, StoryObj } from '@storybook/react'
 
+import DeleteOutline from '../../../assets/icons/delete-outline.tsx'
+import EditOutline from '../../../assets/icons/edit-outline.tsx'
 import Layer from '../../../assets/icons/layer.tsx'
+import LogOut from '../../../assets/icons/log-out.tsx'
+import PersonOutline from '../../../assets/icons/person-outline.tsx'
 
 import { DropdownItem } from './dropdown-item/dropdown-item.tsx'
 import { DropdownMenuDemo } from './dropdown-menu.tsx'
@@ -18,7 +22,13 @@ type Story = StoryObj<typeof meta>
 
 export const DropdownMenuu: Story = {
   args: {
-    children: <DropdownUser />,
+    children: (
+      <div>
+        <DropdownUser />
+        <DropdownItem title={'My Profile'} icon={<PersonOutline color={'white'} />} />
+        <DropdownItem title={'Sign Out'} icon={<LogOut color={'white'} />} />
+      </div>
+    ),
   },
 }
 
@@ -27,6 +37,8 @@ export const DropdownMenu1: Story = {
     children: (
       <div>
         <DropdownItem title={'Learn'} icon={<Layer color={'white'} />} />
+        <DropdownItem title={'Edit'} icon={<EditOutline color={'white'} />} />
+        <DropdownItem title={'Delete'} icon={<DeleteOutline color={'white'} />} />
         {/*<DropdownMenu.Separator style={{ height: '1px', margin: '5px', color: 'black' }} />*/}
         {/*<DropdownItem title={'Edit'} icon={<EditOutline color={'white'} />} />*/}
         {/*<DropdownMenu.Separator style={{ height: '1px', margin: '5px', color: 'darkgray' }} />*/}
--- src/components/ui/dropdown-menu/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu/dropdown-menu.tsx
@@ -1,146 +1,10 @@
-// import { useState } from 'react'
-//
-// import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
-// import {
-//   HamburgerMenuIcon,
-//   DotFilledIcon,
-//   CheckIcon,
-//   ChevronRightIcon,
-// } from '@radix-ui/react-icons'
-//
-// import s from './dropdown-menu.module.scss'
-//
-// export const DropdownMenuDemo = () => {
-//   const [bookmarksChecked, setBookmarksChecked] = useState(true)
-//   const [urlsChecked, setUrlsChecked] = useState(false)
-//   const [person, setPerson] = useState('pedro')
-//
-//   return (
-//     <DropdownMenu.Root>
-//       <DropdownMenu.Trigger asChild>
-//         <button className={s.IconButton} aria-label="Customise options">
-//           <HamburgerMenuIcon />
-//         </button>
-//       </DropdownMenu.Trigger>
-//
-//       <DropdownMenu.Portal>
-//         <DropdownMenu.Content className={s.DropdownMenuContent} sideOffset={5}>
-//           <DropdownMenu.Item className={s.DropdownMenuItem}>
-//             New Tab <div className={s.RightSlot}>⌘+T</div>
-//           </DropdownMenu.Item>
-//           <DropdownMenu.Item className={s.DropdownMenuItem}>
-//             New Window <div className={s.RightSlot}>⌘+N</div>
-//           </DropdownMenu.Item>
-//           <DropdownMenu.Item className={s.DropdownMenuItem} disabled>
-//             New Private Window <div className={s.RightSlot}>⇧+⌘+N</div>
-//           </DropdownMenu.Item>
-//           <DropdownMenu.Sub>
-//             <DropdownMenu.SubTrigger className={s.DropdownMenuSubTrigger}>
-//               More Tools
-//               <div className={s.RightSlot}>
-//                 <ChevronRightIcon />
-//               </div>
-//             </DropdownMenu.SubTrigger>
-//             <DropdownMenu.Portal>
-//               <DropdownMenu.SubContent
-//                 className={s.DropdownMenuSubContent}
-//                 sideOffset={2}
-//                 alignOffset={-5}
-//               >
-//                 <DropdownMenu.Item className={s.DropdownMenuItem}>
-//                   Save Page As… <div className={s.RightSlot}>⌘+S</div>
-//                 </DropdownMenu.Item>
-//                 <DropdownMenu.Item className={s.DropdownMenuItem}>
-//                   Create Shortcut…
-//                 </DropdownMenu.Item>
-//                 <DropdownMenu.Item className={s.DropdownMenuItem}>Name Window…</DropdownMenu.Item>
-//                 <DropdownMenu.Separator className={`${s.DropdownMenu} ${s.Separator}`} />
-//                 <DropdownMenu.Item className={s.DropdownMenuItem}>
-//                   Developer Tools
-//                 </DropdownMenu.Item>
-//               </DropdownMenu.SubContent>
-//             </DropdownMenu.Portal>
-//           </DropdownMenu.Sub>
-//
-//           <DropdownMenu.Separator className={s.DropdownMenuSeparator} />
-//
-//           <DropdownMenu.CheckboxItem
-//             className={s.DropdownMenuCheckboxItem}
-//             checked={bookmarksChecked}
-//             onCheckedChange={setBookmarksChecked}
-//           >
-//             <DropdownMenu.ItemIndicator className={s.DropdownMenuItemIndicator}>
-//               <CheckIcon />
-//             </DropdownMenu.ItemIndicator>
-//             Show Bookmarks <div className={s.RightSlot}>⌘+B</div>
-//           </DropdownMenu.CheckboxItem>
-//           <DropdownMenu.CheckboxItem
-//             className={s.DropdownMenuCheckboxItem}
-//             checked={urlsChecked}
-//             onCheckedChange={setUrlsChecked}
-//           >
-//             <DropdownMenu.ItemIndicator className={s.DropdownMenuItemIndicator}>
-//               <CheckIcon />
-//             </DropdownMenu.ItemIndicator>
-//             Show Full URLs
-//           </DropdownMenu.CheckboxItem>
-//
-//           <DropdownMenu.Separator className={s.DropdownMenuSeparator} />
-//
-//           <DropdownMenu.Label className={s.DropdownMenuLabel}>People</DropdownMenu.Label>
-//           <DropdownMenu.RadioGroup value={person} onValueChange={setPerson}>
-//             <DropdownMenu.RadioItem className={s.DropdownMenuRadioItem} value="pedro">
-//               <DropdownMenu.ItemIndicator className={s.DropdownMenuItemIndicator}>
-//                 <DotFilledIcon />
-//               </DropdownMenu.ItemIndicator>
-//               Pedro Duarte
-//             </DropdownMenu.RadioItem>
-//             <DropdownMenu.RadioItem className={s.DropdownMenuRadioItem} value="colm">
-//               <DropdownMenu.ItemIndicator className={s.DropdownMenuItemIndicator}>
-//                 <DotFilledIcon />
-//               </DropdownMenu.ItemIndicator>
-//               Colm Tuite
-//             </DropdownMenu.RadioItem>
-//           </DropdownMenu.RadioGroup>
-//
-//           <DropdownMenu.Arrow className={s.DropdownMenuArrow} />
-//         </DropdownMenu.Content>
-//       </DropdownMenu.Portal>
-//     </DropdownMenu.Root>
-//   )
-// }
-
 import { ReactNode } from 'react'
 
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
-import DeleteOutline from '../../../assets/icons/delete-outline.tsx'
-import EditOutline from '../../../assets/icons/edit-outline.tsx'
 import user from '../../../assets/icons/funny-user.jpg'
-import Layer from '../../../assets/icons/layer.tsx'
 
-import { DropdownItem } from './dropdown-item/dropdown-item.tsx'
 import s from './dropdown-menu.module.scss'
-import { DropdownUser } from './dropdown-user/dropdown-user.tsx'
-
-// type ItemListProps = {
-//   className?: string
-// }
-// const ItemList = ({ className }: ItemListProps) => {
-//   return (
-//     <div className={className}>
-//       <DropdownItem title={'Learn'}>
-//         <Layer color={'white'} />
-//       </DropdownItem>
-//       <DropdownItem title={'Edit'}>
-//         <EditOutline color={'white'} />
-//       </DropdownItem>
-//       <DropdownItem title={'Delete'}>
-//         <DeleteOutline color={'white'} />
-//       </DropdownItem>
-//     </div>
-//   )
-// }
 
 type DropdownMenuProps = {
   variant?: string
@@ -156,12 +20,10 @@ export const DropdownMenuDemo = ({ variant, children }: DropdownMenuProps) => {
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
         <DropdownMenu.Content className={s.DropdownMenuContent} sideOffset={5}>
-          <DropdownMenu.Item className={s.DropdownMenuItem}>{children}</DropdownMenu.Item>
-          <div className={s.DropdownMenuItem}>{children}</div>
+          {/*<DropdownMenu.Item className={s.DropdownMenuItem}>{children}</DropdownMenu.Item>*/}
+          {/*<div className={s.DropdownMenuItem}>{children}</div>*/}
+          {children}
           <DropdownMenu.Separator className={s.DropdownMenuSeparator} />
-          {/*<DropdownMenu.Item className={s.DropdownMenuItem}>New Window</DropdownMenu.Item>*/}
-          {/*<DropdownMenu.Separator className={s.DropdownMenuSeparator} />*/}
-          {/*<DropdownMenu.Item className={s.DropdownMenuItem}>New Window</DropdownMenu.Item>*/}
           <DropdownMenu.Arrow className={s.DropdownMenuArrow} />
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
